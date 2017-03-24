347de229436596c57b01ea2c9ea041b98cebf91d
Belle <lxliu@student.oaklandcc.edu>
2017-03-24 09:05:37 -0400

    update the quote

--- src/Body.js
+++ src/Body.js
@@ -9,7 +9,7 @@ class Body extends Component{
         <SiteBanner src={'http://mybeadingstudio.com/assets/img_portfolio_banner.jpg'} 
 					alt={'photo of a wild orchid taken near Drummond Island'} />
 		<br />
-		<blockquote>If you haven't found it yet, keep looking. Don&#39;t settle. As with all matters of the heart, you&#39;ll know when you find it. And, like any great relationship, it just gets better and better as the years roll on. -- <em>Steve Job</em>
+		<blockquote>If you haven&#39;t found it yet, keep looking. Don&#39;t settle. As with all matters of the heart, you&#39;ll know when you find it. And, like any great relationship, it just gets better and better as the years roll on. -- <em>Steve Job</em>
 		</blockquote>
       </div>
     );
