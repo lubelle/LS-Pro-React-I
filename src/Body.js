f30cda0db15f2241048350da1cb7cdbcc365ab06
Belle <lxliu@student.oaklandcc.edu>
2017-03-23 22:41:17 -0400

    Update Body.js App.css

--- src/Body.js
+++ src/Body.js
@@ -8,11 +8,8 @@ class Body extends Component{
       <div>
         <SiteBanner src={'http://mybeadingstudio.com/assets/img_portfolio_banner.jpg'} 
 					alt={'photo of a wild orchid taken near Drummond Island'} />
-		<blockquote><p>
-		If you haven't found it yet, keep looking. Don't settle. 
-		As with all matters of the heart, you'll know when you find it. 
-		And, like any great relationship, 
-		it just gets better and better as the years roll on. -- <em>Steve Job</em></p>
+		<br />
+		<blockquote>If you haven't found it yet, keep looking. Don't settle. As with all matters of the heart, you'll know when you find it. And, like any great relationship, it just gets better and better as the years roll on. -- <em>Steve Job</em>
 		</blockquote>
       </div>
     );
